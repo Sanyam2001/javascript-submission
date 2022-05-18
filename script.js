0f4892d19656f28d016724963d365d1226603ea7
Sanyam sharma <98393255+Sanyam2001@users.noreply.github.com>
2022-05-18 17:34:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+alert("Write a javascript program of adding two numbers");
+console.log("enter two numbers");
+let a=+prompt("Enter first number");
+let b=+prompt("enter second number");
+ let c=a+b;
+alert("sum of two numbers is" + c);
+=======================================================================================================================
+*/
+/*
+console.log("JavaScript program to calculate area and perimeter of the rectangle");
+let l=+prompt("enter length of rectangle");
+let b=+prompt("enter breadth of rectangle");
+console.log("Length of the rectangle is : "+l);
+console.log("Breadth of the rectangle is : "+b);
+let a=l*b;
+let p=(2*(l+b));
+console.log("Area is : "+a+" Perimeter is : "+p);
+=======================================================================================================================
+*/
+console.log("JavaScript program to calculate payble amount by customer for burger and pizza");
+let b=+prompt("enter number of burgers");
+let p=+prompt("enter number of pizzas");
+console.log("Number of burgers : "+b);
+console.log("Number of pizzas : "+p);
+let amB=+(45*b);
+let amP=+(250*p);
+let total=amP+amB;
+console.log("Amount for burgers : "+amB+" Amount for pizzas : "+amP);
+console.log("Total Amount Payble : "+total);
